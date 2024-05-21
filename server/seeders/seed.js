65759e57630e8388ce6f5ea47290c45ce06aabba
courtneyhook <118090383+courtneyhook@users.noreply.github.com>
2024-05-21 13:11:46 -0500

    seed.js is complete

---
+++ server/seeders/seed.js
@@ -0,0 +1,16 @@
+const db = require("../config/connection");
+const { Profile } = require("../models");
+const profileSeeds = require("./profileseeds");
+const cleanDB = require("./cleanDB");
+
+db.once("open", async () => {
+  try {
+    await cleanDB("Profile", "profiles");
+
+    await Profile.create(profileSeeds);
+
+    process.exit(0);
+  } catch (error) {
+    throw error;
+  }
+});
