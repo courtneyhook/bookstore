c212a5dbd89e4ffadec3b8150a1a34774136352a
courtneyhook <118090383+courtneyhook@users.noreply.github.com>
2024-05-21 13:23:43 -0500

    cleanDB complete

---
+++ server/seeders/profileseeds.js
@@ -0,0 +1,13 @@
+const { model } = require("mongoose");
+
+const books = [
+  {
+    name: "A Bugs Life",
+    price: 9.95,
+    quantity: 15,
+    author: "Author Name",
+    published: 1995,
+  },
+];
+
+module.exports = books;
